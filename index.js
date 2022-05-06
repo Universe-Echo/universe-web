8b62eeeb3abc63aa71d8eda26b855e66b905ae14
universe-echo <ushabhati0202@gmail.com>
2022-05-06 21:43:46 +0530

    hello

--- index.js
+++ index.js
@@ -29,4 +29,6 @@ const path = require('path')
   })
 
 
-  app.listen(port)
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
